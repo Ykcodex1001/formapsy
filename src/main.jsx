38ca5c80c38be2c94c57396fb0ff31d78eb25af7
Roubache Younes <younesroubache20@gmail.com>
2026-02-17 22:38:47 +0100

    Integrate Facebook Pixel tracking
    Added Facebook Pixel tracking code to main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+!(function (f, b, e, v, n, t, s) {
+  if (f.fbq) return;
+  n = f.fbq = function () {
+    n.callMethod
+      ? n.callMethod.apply(n, arguments)
+      : n.queue.push(arguments);
+  };
+  if (!f._fbq) f._fbq = n;
+  n.push = n;
+  n.loaded = true;
+  n.version = "2.0";
+  n.queue = [];
+  t = b.createElement(e);
+  t.async = true;
+  t.src = v;
+  s = b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t, s);
+})(window, document, "script",
+  "https://connect.facebook.net/en_US/fbevents.js");
+
+window.fbq("init", "1438555634639300");
+window.fbq("track", "PageView");
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
